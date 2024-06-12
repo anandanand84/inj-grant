4a2579475f3e9d806a7ec86300bae15985c402eb
Anand Aravindan <aaravindan@dvtrading.co>
2024-06-12 11:28:52 -0500

    Fix proxy

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,30 +1,40 @@
 import type { Handle } from "@sveltejs/kit";
+import { Agent } from 'undici';
+import { constants } from 'crypto'
 
-const handleApiProxy: Handle = async ({ event }) => {
+let agent = new Agent({
+    connect: {
+      rejectUnauthorized: false,
+      secureOptions: constants.SSL_OP_LEGACY_SERVER_CONNECT
+    }
+  })
+const handleApiProxy: Handle = async ({ event, resolve }) => {
   // strip `/api-proxy` from the request path
-  let newurl = event.request.url.replace(/(https?:\/\/[^\/]+)\/cosmos/, 'https://sentry.lcd.injective.network/cosmos');
+    let newurl = event.request.url.replace(/(https?:\/\/[^\/]+)\/cosmos/, 'https://sentry.lcd.injective.network/cosmos');
     console.log("new url", newurl);
+    // Strip off header added by SvelteKit yet forbidden by underlying HTTP request
+    // library `undici`.
+    // https://github.com/nodejs/undici/issues/1470
+    event.request.headers.delete("connection");
+    event.request.headers.delete("host");
+    event.request.headers.delete("referer");
+    
     let request = new Request(
         newurl,
         event.request
     );
-
-  const proxiedUrl = new URL(newurl);
-
-  // Strip off header added by SvelteKit yet forbidden by underlying HTTP request
-  // library `undici`.
-  // https://github.com/nodejs/undici/issues/1470
-  event.request.headers.delete("connection");
-
-  return fetch(proxiedUrl.toString(), {
+    let result = event.fetch(request, {
     // propagate the request method and body
     body: event.request.body,
     method: event.request.method,
     headers: event.request.headers,
+    
   }).catch((err) => {
     console.log("Could not proxy API request: ", err);
     throw err;
   });
+
+  return result;
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
