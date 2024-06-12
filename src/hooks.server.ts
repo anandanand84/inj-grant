29f1d3df45b6f549bccb5b5b19d872f4a00c52f8
Anand Aravindan <aaravindan@dvtrading.co>
2024-06-12 10:17:18 -0500

    Try proxy 2

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,43 +1,37 @@
-/** @type {import('@sveltejs/kit').HandleFetch} */
-export async function handleFetch({ request, fetch }) {
-    console.log("Handling fetch");
-    console.log(request);
-	if (request.url.includes('/cosmos/')) {
-        // clone the original request, but change the URL
-        console.log(request.url);
-        let newurl = request.url.replace(/(https?:\/\/[^\/]+)\/cosmos/, 'https://sentry.lcd.injective.network/cosmos');
-        console.log(newurl);
-        request = new Request(
-            newurl,
-            request
-        );
-    }
-    
-    request.url
+import { type Handle } from "@sveltejs/kit";
 
-	return fetch(request);
-}
+const handleApiProxy: Handle = async ({ event }) => {
+  // strip `/api-proxy` from the request path
+  let newurl = event.request.url.replace(/(https?:\/\/[^\/]+)\/cosmos/, 'https://sentry.lcd.injective.network/cosmos');
+    console.log("new url", newurl);
+    let request = new Request(
+        newurl,
+        event.request
+    );
 
-export async function handle({ event, resolve }) {
-    console.log("Hello");
-    if (event.url.pathname === '/ping') {
-        return new Response('pong');
-	}
-    if (event.url.pathname.includes('/cosmos/')) {
-        console.log("Hello 1");
-        let newurl = event.request.url.replace(/(https?:\/\/[^\/]+)\/cosmos/, 'https://sentry.lcd.injective.network/cosmos');
-        console.log("new url", newurl);
-        let request = new Request(
-            newurl,
-            event.request
-        );
-        try {
-            let result = await fetch(request);
-            console.log(result);
-            return resolve(await result.json());
-        } catch(err) {
-            console.error(err);
-        }
-    }
-	return await resolve(event);
-}
+  const proxiedUrl = new URL(newurl);
+
+  // Strip off header added by SvelteKit yet forbidden by underlying HTTP request
+  // library `undici`.
+  // https://github.com/nodejs/undici/issues/1470
+  event.request.headers.delete("connection");
+
+  return fetch(proxiedUrl.toString(), {
+    // propagate the request method and body
+    body: event.request.body,
+    method: event.request.method,
+    headers: event.request.headers,
+  }).catch((err) => {
+    console.log("Could not proxy API request: ", err);
+    throw err;
+  });
+};
+
+export const handle: Handle = async ({ event, resolve }) => {
+  // intercept requests to `/api-proxy` and handle them with `handleApiProxy`
+  if (event.url.pathname.includes('/cosmos/')) {
+    return await handleApiProxy({ event, resolve });
+  }
+
+  return await resolve(event);
+};
